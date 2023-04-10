4ec1907d858619d81d5df434b6b3ab31fbc1c097
gustavo-silva-clear <gustavo.silva.clear@gamil.com>
2023-04-10 08:11:10 -0300

    feat: Configuration: NextJS, TailwindCSS and Firebase part-2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
-
 export default function Home() {
   return (
-    <div >
+    <div className={`
+    flex 
+    h-screen
+     justify-center 
+     items-center
+     bg-gradient-to-r from-purple-950 to-red-700
+     `}>
 
+    <span className='text-5xl text-white'>Text</span>
 
     </div>
   )
